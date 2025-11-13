69a0905ce64c4da70bea3f1b404bea85056f0ad4
ExoSkeleton <calazulazu@gmail.com>
2025-11-13 12:16:18 +0200

    Initial commit

---
+++ app/src/main/java/com/eanie/mealy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.eanie.mealy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eanie.mealy.ui.kitchen.KitchenFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.container, KitchenFragment.newInstance())
+					.commitNow();
+		}
+	}
+}
---
+++ app/src/main/java/com/eanie/mealy/ui/kitchen/KitchenViewModel.java
@@ -0,0 +1,7 @@
+package com.eanie.mealy.ui.kitchen;
+
+import androidx.lifecycle.ViewModel;
+
+public class KitchenViewModel extends ViewModel {
+	// TODO: Implement the ViewModel
+}
