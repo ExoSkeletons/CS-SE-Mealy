c821cc8923e2a40f409f38fd9b994539b7c0d8a9
ExoSkeleton <calazulazu@gmail.com>
2025-12-01 18:12:19 +0200

    Fragment uses viewmodel, add items button

--- app/src/main/java/com/eanie/mealy/ui/kitchen/KitchenFragment.java
+++ app/src/main/java/com/eanie/mealy/ui/kitchen/KitchenFragment.java
@@ -5,6 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.eanie.mealy.Ingredient;
+import com.eanie.mealy.Quantity;
+import com.eanie.mealy.R;
+import com.eanie.mealy.UnitType;
+import com.eanie.mealy.models.ItemsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,23 +21,18 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.eanie.mealy.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class KitchenFragment extends Fragment {
 
 	public static KitchenFragment newInstance() {
 		return new KitchenFragment();
 	}
 
-	private KitchenViewModel mViewModel;
+	private ItemsViewModel mViewModel;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mViewModel = new ViewModelProvider(this).get(KitchenViewModel.class);
+		mViewModel = new ViewModelProvider(this).get(ItemsViewModel.class);
 		// TODO: Use the ViewModel
 	}
 
@@ -39,22 +43,34 @@ public class KitchenFragment extends Fragment {
 		return inflater.inflate(R.layout.fragment_kitchen, container, false);
 	}
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
 
-        RecyclerView stock_list = view.findViewById(R.id.stock_rv);
+		RecyclerView stock_list = view.findViewById(R.id.stock_rv);
 
-        // Create sample data
-        List<KitchenItem> kitchenItems = new ArrayList<>();
-        kitchenItems.add(new KitchenItem("Apples", 5, R.mipmap.ic_launcher)); // Using a default drawable for now
-        kitchenItems.add(new KitchenItem("Bananas", 3, R.mipmap.ic_launcher));
-        kitchenItems.add(new KitchenItem("Milk", 1, R.mipmap.ic_launcher));
-        kitchenItems.add(new KitchenItem("Bread", 2, R.mipmap.ic_launcher));
+		// Create sample data
+		List<KitchenItem> kitchenItems = new ArrayList<>();
+		kitchenItems.add(new KitchenItem(new Ingredient("ing_apple"), new Quantity(5))); // Using a default drawable for now
+		kitchenItems.add(new KitchenItem(new Ingredient("ing_cheese"), new Quantity(200, UnitType.GRAMS)));
+		kitchenItems.add(new KitchenItem(new Ingredient("ing_cucumber"), new Quantity(3)));
+		kitchenItems.add(new KitchenItem(new Ingredient("ing_milk"), new Quantity(1.5, UnitType.LITERS)));
+		kitchenItems.add(new KitchenItem(new Ingredient("ing_bread"), new Quantity(2000, UnitType.GRAMS)));
 
+		// This user id is tempory (it is just my fake user) -Aviad
+		mViewModel.setUserId("lvwuK3xBNufRynvXdB8XRqirziu2");
+		/*view.findViewById(R.id.imageButton).setOnClickListener(v -> {
+			for (KitchenItem item : kitchenItems)
+				mViewModel.addIngredient(item);
+		});*/
+		// Get items live with -
+		mViewModel.myItems().observe(getViewLifecycleOwner(), items -> {
 
-        KitchenAdapter adapter = new KitchenAdapter(kitchenItems);
-        stock_list.setAdapter(adapter);
-        stock_list.setLayoutManager(new GridLayoutManager(getContext(), 2)); // 2 columns in the grid
-    }
+		});
+
+		// TODO: switch to use RecyclerView ListAdapter!!!! for kitchen adapter
+		KitchenAdapter adapter = new KitchenAdapter(kitchenItems);
+		stock_list.setAdapter(adapter);
+		stock_list.setLayoutManager(new GridLayoutManager(getContext(), 2)); // 2 columns in the grid
+	}
 }
