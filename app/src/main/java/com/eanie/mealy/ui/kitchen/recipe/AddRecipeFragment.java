00d963a6d6757058256496063ae1e94bff6c6696
EmunaSofer <emuna002@gmail.com>
2026-01-12 16:35:54 +0200

    added RecipeAddViewModel

--- app/src/main/java/com/eanie/mealy/ui/kitchen/recipe/AddRecipeFragment.java
+++ app/src/main/java/com/eanie/mealy/ui/kitchen/recipe/AddRecipeFragment.java
@@ -1,44 +1,46 @@
 package com.eanie.mealy.ui.kitchen.recipe;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.eanie.mealy.R;
-import com.eanie.mealy.Recipe;
-import com.eanie.mealy.data.RecipeRepo;
-import com.eanie.mealy.ui.kitchen.KitchenItem;
+import com.eanie.mealy.models.RecipeAddViewModel;
+import com.eanie.mealy.ui.kitchen.KitchenItemAdapter;
 
 import java.util.ArrayList;
 
 public class AddRecipeFragment extends Fragment {
-
-    private EditText etName;
-    private EditText etInstructions;
-    private Button btnSave;
-    private Button btnCancel;
-
-    private RecipeRepo recipeRepo;
-
-    public AddRecipeFragment() { }
+    public AddRecipeFragment() {
+    }
 
     public static AddRecipeFragment newInstance() {
         return new AddRecipeFragment();
     }
+
+    private RecipeAddViewModel recipeAddViewModel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        recipeAddViewModel = new ViewModelProvider(this).get(RecipeAddViewModel.class);
+
         return inflater.inflate(R.layout.fragment_add_recipe, container, false);
     }
 
@@ -46,12 +48,55 @@ public class AddRecipeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        etName = view.findViewById(R.id.et_recipe_name);
-        etInstructions = view.findViewById(R.id.et_instructions);
-        btnSave = view.findViewById(R.id.btn_save_recipe);
-        btnCancel = view.findViewById(R.id.btn_cancel);
+        EditText etName = view.findViewById(R.id.et_recipe_name);
+        EditText etInstructions = view.findViewById(R.id.et_instructions);
+        Button btnSave = view.findViewById(R.id.btn_save_recipe);
+        ImageButton btnCancel = view.findViewById(R.id.btn_close);
 
-        recipeRepo = new RecipeRepo();
+        RecyclerView rvIngredients = view.findViewById(R.id.rv_ingredients);
+        KitchenItemAdapter ingredientAdapter = new KitchenItemAdapter(false);
+        ingredientAdapter.submitList(new ArrayList<>());
+        rvIngredients.setAdapter(ingredientAdapter);
+        rvIngredients.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false));
+
+        recipeAddViewModel.ingredients.observe(getViewLifecycleOwner(), ingredientAdapter::submitList);
+
+        Button btnAddIngredient = view.findViewById(R.id.btn_add_ingredient);
+        btnAddIngredient.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Add ingredient", Toast.LENGTH_SHORT).show();
+            // todo: show dialog to add ingredient
+        });
+
+        etName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                recipeAddViewModel.name.postValue(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+        });
+        etInstructions.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                recipeAddViewModel.instructions.postValue(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
 
         btnSave.setOnClickListener(v -> saveRecipe());
 
@@ -62,30 +107,7 @@ public class AddRecipeFragment extends Fragment {
     }
 
     private void saveRecipe() {
-        String name = etName.getText().toString().trim();
-        String instructions = etInstructions.getText().toString().trim();
-
-        if (name.isEmpty()) {
-            etName.setError("Please enter a recipe name");
-            return;
-        }
-        if (instructions.isEmpty()) {
-            etInstructions.setError("Please enter instructions");
-            return;
-        }
-
-
-        String chefId = "demo-user";
-
-        Recipe newRecipe = new Recipe(
-                null,
-                name,
-                instructions,
-                new ArrayList<KitchenItem>(),
-                chefId
-        );
-
-        recipeRepo.insert(newRecipe);
+        recipeAddViewModel.saveRecipe();
 
         Toast.makeText(getContext(), "Recipe saved!", Toast.LENGTH_SHORT).show();
 
