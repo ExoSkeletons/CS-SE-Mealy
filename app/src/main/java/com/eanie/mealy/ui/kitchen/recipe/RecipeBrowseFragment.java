7e5846ba296be3cc78f3a25c01b520a2a140d8b4
ExoSkeleton <calazulazu@gmail.com>
2026-01-13 22:21:02 +0200

    added userItemsVM

--- app/src/main/java/com/eanie/mealy/ui/kitchen/recipe/RecipeBrowseFragment.java
+++ app/src/main/java/com/eanie/mealy/ui/kitchen/recipe/RecipeBrowseFragment.java
@@ -5,6 +5,7 @@ import android.view.View;
 
 import com.eanie.mealy.R;
 import com.eanie.mealy.models.DiscoveryViewModel;
+import com.eanie.mealy.models.UserItemsViewModel;
 
 import java.util.List;
 
@@ -12,18 +13,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
+import static com.eanie.mealy.models.UserDataViewModel.ARG_UUID;
 import static com.eanie.mealy.models.UserDataViewModel.withUserId;
 
 public class RecipeBrowseFragment extends RecipeListFragment {
 	private DiscoveryViewModel discoveryVM;
+	private UserItemsViewModel userItemsVM;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		discoveryVM = getDefaultViewModelProviderFactory().create(DiscoveryViewModel.class);
 
 		var provider = new ViewModelProvider(requireActivity());
 		discoveryVM = provider.get(DiscoveryViewModel.class);
+		userItemsVM = provider.get(UserItemsViewModel.class);
+
+		if (getArguments() != null) {
+			var userId = getArguments().getString(ARG_UUID, null);
+			if (userId != null) userItemsVM.setUserId(userId);
+		}
 	}
 
 	@Override
@@ -46,6 +54,9 @@ public class RecipeBrowseFragment extends RecipeListFragment {
 
 	@Override
 	protected void observeData() {
+		userItemsVM.myItems().observe(getViewLifecycleOwner(), items -> {
+			if (items != null) discoveryVM.updateIngredients(items);
+		});
 		discoveryVM.makeableRecipes().observe(getViewLifecycleOwner(), recipes -> {
 			if (recipes == null) adapter().submitList(List.of());
 			else adapter().submitList(recipes);
